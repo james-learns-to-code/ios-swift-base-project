c91696ed93b80db87301fca4be046fef3b32770d
jameslee <jameslee@goodeffect.com>
2019-06-12 20:32:32 +0900

    Remove pod folder.

--- Pods/CocoaAnalyzer/Pods/Target Support Files/Pods-IBAnalyzer-IBAnalyzerTests/Pods-IBAnalyzer-IBAnalyzerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IBAnalyzer_IBAnalyzerTests : NSObject
-@end
-@implementation PodsDummy_Pods_IBAnalyzer_IBAnalyzerTests
-@end
--- Pods/CocoaAnalyzer/Pods/Target Support Files/Pods-IBAnalyzer/Pods-IBAnalyzer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_IBAnalyzer : NSObject
-@end
-@implementation PodsDummy_Pods_IBAnalyzer
-@end
